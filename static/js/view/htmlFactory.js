d3b5392725e75905fdde3ad1b4a384467ae359de
Karolina Chalupka <k.s.chalupka@gmail.com>
2022-06-14 09:15:43 +0200

    data added to section board

--- static/js/view/htmlFactory.js
+++ static/js/view/htmlFactory.js
@@ -21,31 +21,13 @@ export function htmlFactory(template) {
 }
 
 function boardBuilder(board) {
-    return `<section class="board">
+    return `<section class="board" data-board-id=${board.id}>
                 <div class="board-header" data-board-id=${board.id}>
                     <span class="board-title">${board.title}</span>
                     <button class="board-add">Add Card</button>
                     <button class="board-toggle" data-board-id="${board.id}">
                         <i class="fas fa-chevron-down"></i>
                    </button>
-                </div>
-                <div class="board-columns">
-                    <div class="board-column">
-                        <div class="board-column-title">New</div>
-                        <div class="board-column-content"></div>
-                    </div>
-                    <div class="board-column">
-                        <div class="board-column-title">In Progress</div>
-                        <div class="board-column-content"></div>
-                    </div>
-                    <div class="board-column">
-                        <div class="board-column-title">Testing</div>
-                        <div class="board-column-content"></div>
-                    </div>
-                    <div class="board-column">
-                        <div class="board-column-title">Done</div>
-                        <div class="board-column-content"></div>
-                    </div>
                 </div>
             </section>`
 }
